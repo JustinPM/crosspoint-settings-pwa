d901edf72de52cfd48842a8c968cdb86d5885118
JustinPM <dr.wierd@gmail.com>
2026-01-18 19:05:39 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,41 @@
-const CACHE_NAME = 'crosspoint-v1';
+const CACHE_NAME = 'crosspoint-v2';
 const ASSETS = [
-  './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './sw.js'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+// Install: Cache basic assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// Fetch: Network First Strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // If network works, update the cache
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, resClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request)) // If offline, use cache
   );
 });
